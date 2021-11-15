ae2478253a6b8a909c941ca6bf0357d7bee9e8d6
mhmdmirw <mhmdmir992@gmail.com>
2021-11-15 16:29:24 +0330

    this is a change of html files

--- tumlai/views.py
+++ tumlai/views.py
@@ -2,4 +2,4 @@ from django.contrib.auth import authenticate, login, get_user_model
 from django.shortcuts import render, redirect
 
 def home_page(request):
-    return render(request, 'home-2.html',{})
+    return render(request, 'index.html',{})
