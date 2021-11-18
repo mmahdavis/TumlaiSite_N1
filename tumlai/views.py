e7eee8f1bd17376d62b3ffd1d4a430776b427a8d
mmahdavis <mmahdavis20@gmail.com>
2021-11-18 20:02:39 +0330

    blogs and blog_detail is reading from mysql

--- tumlai/views.py
+++ tumlai/views.py
@@ -1,20 +1,31 @@
 from django.contrib.auth import authenticate, login, get_user_model
 from django.shortcuts import render, redirect
+from blog.models import Post, PostConnectToCategory, Category
+from django.db.models import Q
+
 
 def home_page(request):
-    return render(request, 'index.html',{})
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
 
-def contact_us (request):
-    return render(request,'contact_us.html',{})
+
+def contact_us(request):
+    return render(request, 'contact_us.html', {})
+
 
 def about_us(request):
-    return render(request, 'about_us.html',{})
+    return render(request, 'about_us.html', {})
+
 
 def not_found(request):
-    return render(request, '404.html',{})
+    return render(request, '404.html', {})
 
-def blog_details(request):
-    return render(request,'blog_details.html',{})
+
+def blog_details(request, pk):
+    posts = Post.objects.get(id = pk)
+    return render(request, 'blog_details.html', {'posts': posts})
+
 
 def blog_list(request):
-    return render(request,'tumlai_blog.html',{})
+    posts = Post.objects.all()
+    return render(request,'tumlai_blog.html', {'posts': posts})
